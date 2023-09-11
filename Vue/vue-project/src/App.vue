304758fc70f30438ef7b426888f2c69b308fc485
cc-wangxin2020 <cc_wangxin2020@163.com>
2023-09-11 14:01:35 +0800

    练习

--- Vue/vue-project/src/App.vue
+++ Vue/vue-project/src/App.vue
@@ -13,8 +13,57 @@ const current = ref(0)  // 0 表示球员， 1 表示球队
     </header>
     <!-- 选项卡主体 -->
     <div class="main">
-      <div>球员</div>
-      <div>球队</div>
+      <div v-show="current === 0">
+        <!-- 球员 -->
+        <div class="tab_list">
+          <div class="tab_item">
+            <!-- 图片 -->
+            <div class="photo">
+              <img src="/images/780.jpg" alt="">
+              <span>1</span>
+            </div>
+            <!-- 描述 -->
+            <div class="desc">
+              <div class="name">梅西</div>
+              <div class="hot-bar">
+                <div class="inner">433760热度</div>
+              </div>
+            </div>
+          </div>
+          <div class="tab_item">
+            <!-- 图片 -->
+            <div class="photo">
+              <img src="/images/780.jpg" alt="">
+              <span>1</span>
+            </div>
+            <!-- 描述 -->
+            <div class="desc">
+              <div class="name">梅西</div>
+              <div class="hot-bar">
+                <div class="inner">433760热度</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-show="current === 1">
+        <div class="tab_list">
+          <div class="tab_item">
+            <!-- 图片 -->
+            <div class="photo">
+              <img src="/images/781.jpg" alt="">
+              <span>1</span>
+            </div>
+            <!-- 描述 -->
+            <div class="desc">
+              <div class="name">法国队</div>
+              <div class="hot-bar">
+                <div class="inner">433760热度</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -46,5 +95,62 @@ const current = ref(0)  // 0 表示球员， 1 表示球队
 .active {
   background-color: #BB0334;
   color: #fff;
+}
+.tab_list{
+  margin: 20px;
+}
+.tab_item{
+  /* 将item中的图片描述水平排列 */
+  display: flex;
+  margin: 20px;
+}
+.photo{
+  position: relative;
+  width: 150px;
+  border-radius: 15px;
+  overflow: hidden;
+  background-color: #bfa;
+}
+.photo img{
+  width: 100%;
+  vertical-align: middle;
+}
+.photo span{
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  background-color: rgb(245, 102, 1);
+  left: 0;
+  top: 0;
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom-right-radius: 15px;
+}
+.desc{
+  display: flex;
+  flex-flow: column;
+  justify-content: space-evenly;
+  color: #fff;
+  font-size: 30px;
+  flex: auto;
+  margin-left: 30px;
+}
+.hot-bar{
+  background-color: rgb(3, 37, 103);
+  border-radius: 20px;
+  /* 表示首行文本的缩进量 */
+  text-indent: .5em;
+}
+.inner{
+  background-color: #bfa;
+  border-radius: 20px;
+  width: 10%;
+  background-image: linear-gradient(to right, rgb(187, 3, 52) 50%, rgb(66, 2, 12));
+  /* 设置文字不换行 */
+  white-space: nowrap;
 }
 </style>
--- Vue/使用vite/vue-project/src/App.vue
+++ Vue/使用vite/vue-project/src/App.vue
@@ -1,19 +1,7 @@
 <script setup>
-import myBox from './components/myBox.vue'
+import { ref } from 'vue';
+const isShow = ref(true)
 </script>
 <template>
-    <h1>标题一</h1>
-    <myBox></myBox>
+    <component is="h1">动态组件</component>
 </template>
-<!-- 可以为style标签添加一个scoped属性，这样样式成为局部变量，只对当前组件生效
-    实现：
-        - 当在组件中使用scoped属性时，vue会自动为组件中的所有元素生成一个随机的属性，
-          生成后所有的选择器都会在最后添加一个该属性（属性选择器）
-        - 随机生成的属性，除了会添加到当前组件内的所有元素上，
-        也会随机添加加到当前组件引入的其他组件的根元素上
--->
-<style scoped>
-h1{
-    background-color: #bfa;
-}
-</style>
